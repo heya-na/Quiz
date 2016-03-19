7803e56d98ee7187ccd9041b40dbdf30e7e5f309
Heya Na <naheya1126@gmail.com>
2016-03-18 20:58:15 -0400

    Added quiz object

---
+++ quiz.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Heya on 2016-03-18.
+ */
+function Quiz(questions){
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+    this.score = 0;
+}
+
+Quiz.prototype.choose = function(answer){
+    if(this.getCurrentQuestion().isAnswerCorrect(answer)){
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+}
+
+Quiz.prototype.getCurrentQuestion = function(){
+    return this.questions[this.currentQuestionIndex];
+}
+
+Quiz.prototype.hasEnded = function(){
+    return this.currentQuestionIndex >= this.questions.length;
+}
