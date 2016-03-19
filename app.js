dd813396151ace9b0a4e759253d259e98526789b
Heya Na <naheya1126@gmail.com>
2016-03-18 21:13:48 -0400

    Added app.js to create instance for questions and quiz

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Heya on 2016-03-18.
+ */
+//create questions
+var questions = [
+  new Question("How many oscars did the Titanic movie got?", ["9", "10", "11", "12"], "11"),
+    new Question("What is the house number of the Simpsons?", ["146", "742", "720", "782"], "742")
+];
+
+//create quiz
+var quiz = new Quiz(questions);
